c6eb6cbfecbcad63786f2e64984409454dfa09e7
yukey1240 <dorinko192@gmail.com>
2020-10-16 13:29:49 +0900

    キャラクターと宝箱の初期配置をランダムにした

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Content;
     public GameObject runButton;
+    public GameObject box;
+    public GameObject player;
 
     public PlayerManager playerSclipt;
 
@@ -17,9 +22,52 @@ public class GameManager : MonoBehaviour
     [System.NonSerialized]
     public List<string> cmdList = new List<string>();
 
+    List<int> NumList = new List<int>();
+
+    private void Start()
+    {
+        NumList.Add(0);
+        NumList.Add(1);
+        NumList.Add(2);
+
+        box.transform.position = setStartPosition();
+        player.transform.position = setStartPosition();
+    }
+
     void Update()
     {
 
+    }
+
+    Vector3 setStartPosition()
+    {
+        Vector3 pos = Vector3.zero;
+
+        // NumListのシャッフル
+        NumList = NumList.OrderBy(a => Guid.NewGuid()).ToList();
+
+        switch (NumList[0])
+        {
+            case 0:
+                // 右上の出現座標を返す
+                pos = new Vector3(1.88f, 4.05f, 0.0f);
+                NumList[0] = UnityEngine.Random.Range(0, 3);
+                NumList.RemoveAt(0);
+                return pos;
+            case 1:
+                // 左上の出現座標を返す
+                pos = new Vector3(-1.88f, 4.05f, 0.0f);
+                NumList[0] = UnityEngine.Random.Range(0, 3);
+                NumList.RemoveAt(0);
+                return pos;
+            case 2:
+                // 左下の出現座標を返す
+                pos = new Vector3(-1.88f, 0.25f, 0.0f);
+                NumList[0] = UnityEngine.Random.Range(0, 3);
+                NumList.RemoveAt(0);
+                return pos;
+        }
+        return pos;
     }
 
     public void RunButtonClick()
@@ -29,10 +77,10 @@ public class GameManager : MonoBehaviour
 
         Image dropImage;
         // Framesの子のFremeImage1～Nまでを取得
-        for (int i = 0; i < 4; i++)
+        for (var i = 0; i < 4; i++)
         {
             // FremeImage1～20まで子のDropImageを取得
-            string frameImageName = $"FrameImage_{i + 1}";
+            var frameImageName = $"FrameImage_{i + 1}";
             dropImage = Content.transform.Find(frameImageName).GetChild(0).gameObject.GetComponent<Image>();
             
             // print(frames.transform.GetChild(i).transform.GetChild(0).name);
