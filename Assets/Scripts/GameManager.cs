5bca5f7d1ff2693c1bc1a69b33383de105690207
yukey1240 <dorinko192@gmail.com>
2020-10-23 14:58:46 +0900

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,13 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Content;
     public GameObject runButton;
+    public GameObject reTryPanel;
 
     private PlayerManager playerSclipt;
 
@@ -24,9 +25,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         // データのセーブ
-        // SaveData.Instance.Save();
-
-        
+        // SaveData.Instance.Save();      
     }
 
     void Update()
@@ -68,7 +67,17 @@ public class GameManager : MonoBehaviour
 
         }
         // 一連のコマンド情報を猫に渡す
-        playerSclipt.ReceaveCmd(cmdList);
+        playerSclipt.ReceaveCmd(cmdList, reTryPanel);
+    }
+
+    public void ReTry()
+    {
+        reTryPanel.SetActive(false);
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene("Main");
     }
 
 }
