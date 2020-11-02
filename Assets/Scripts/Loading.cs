15c1f80af44700c31e8cdda1c00d978007074d04
yukey1240 <dorinko192@gmail.com>
2020-11-02 15:17:39 +0900

    スタード画面 → ゲーム画面移行時のロード表示をつけた。

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+	//　非同期動作で使用するAsyncOperation
+	private AsyncOperation async;
+
+	//　シーンロード中に表示するUI画面
+	[SerializeField]
+	private GameObject loadUI;
+
+	//　読み込み率を表示するスライダー
+	[SerializeField]
+	private Slider slider;
+
+	public void NextScene()
+	{
+		//　ロード画面UIをアクティブにする
+		loadUI.SetActive(true);
+
+		//　コルーチンを開始
+		StartCoroutine("LoadData");
+	}
+
+	IEnumerator LoadData()
+	{
+		// シーンの読み込みをする
+		async = SceneManager.LoadSceneAsync("Main");
+
+		//　読み込みが終わるまで進捗状況をスライダーの値に反映させる
+		while (!async.isDone)
+		{
+			var progressVal = Mathf.Clamp01(async.progress / 0.9f);
+			slider.value = progressVal;
+			yield return null;
+		}
+	}
+}
